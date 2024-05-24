1d3f61ea44cb98b45d1c739a269f533ea2ae9b44
hnata030 <127332480+hnata030@users.noreply.github.com>
2024-05-24 09:56:20 +0400

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Lab2.Models
+{
+    public class DataBaseModel
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Имя")]
+        [DisplayName("Имя")]
+        public string Name { get; set; }
+
+        [Display(Name = "Адрес")]
+        [DisplayName("Адрес")]
+        public string Address { get; set; }
+
+        [Display(Name = "Дата рождения")]
+        [DisplayName("Дата рождения")]
+        public DateTime BirthDate { get; set; }
+
+        [Display(Name = "Email")]
+        [DisplayName("Email")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Display(Name = "Номер телефона")]
+        [DisplayName("Номер телефона")]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "Количество вакансий")]
+        [DisplayName("Количество вакансий")]
+        public int VacancyCount { get; set; }
+    }
+}
---
+++ DataController.cs
@@ -0,0 +1,28 @@
+﻿using Lab2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static List<DataBaseModel> databaseData = new List<DataBaseModel>();
+
+        public ActionResult Index()
+        {
+            var model = new DataBaseModel();
+            return View(model);
+        }
+        public ActionResult Add()
+        {
+            var newDatabase = new DataBaseModel();
+            return View(newDatabase);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(DataBaseModel model)
+        {
+            // Код сохранения изменений
+            return RedirectToAction("Index");
+        }
+    }
+}
